cf82192393985995e4d599e71711b805761e6a43
Marcelo Buongermino <buon.marcelo@hotmail.com>
2020-09-11 13:08:11 -0300

    Dispatch bot com LUIS e QnA

--- index.js
+++ index.js
@@ -14,29 +14,23 @@ const restify = require('restify');
 // See https://aka.ms/bot-services to learn more about the different parts of a bot.
 const { BotFrameworkAdapter } = require('botbuilder');
 
-// This bot's main dialog.
-const { EchoBot } = require('./bot');
+// Dialogo Padrão.
+// const { LGPDBot } = require('./bot');
 
-// Create HTTP server
-const server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, () => {
-    console.log(`\n${ server.name } listening to ${ server.url }`);
-    console.log('\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator');
-    console.log('\nTo talk to your bot, open the emulator select "Open Bot"');
-});
+// teste
+const { DispatchBot } = require('./DispatchBot');
 
-// Create adapter.
-// See https://aka.ms/about-bot-adapter to learn more about how bots work.
 const adapter = new BotFrameworkAdapter({
     appId: process.env.MicrosoftAppId,
     appPassword: process.env.MicrosoftAppPassword
 });
 
 // Catch-all for errors.
-const onTurnErrorHandler = async (context, error) => {
+adapter.onTurnError = async (context, error) => {
     // This check writes out errors to console log .vs. app insights.
     // NOTE: In production environment, you should consider logging this to Azure
-    //       application insights.
+    //       application insights. See https://aka.ms/bottelemetry for telemetry
+    //       configuration instructions.
     console.error(`\n [onTurnError] unhandled error: ${ error }`);
 
     // Send a trace activity, which will be displayed in Bot Framework Emulator
@@ -52,11 +46,46 @@ const onTurnErrorHandler = async (context, error) => {
     await context.sendActivity('To continue to run this bot, please fix the bot source code.');
 };
 
+// Create HTTP server
+const server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, () => {
+    console.log(`\n${ server.name } listening to ${ server.url }`);
+    console.log('\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator');
+    console.log('\nTo talk to your bot, open the emulator select "Open Bot"');
+});
+
+// Create adapter.
+// See https://aka.ms/about-bot-adapter to learn more about how bots work.
+// const adapter = new BotFrameworkAdapter({
+//     appId: process.env.MicrosoftAppId,
+//     appPassword: process.env.MicrosoftAppPassword
+// });
+
+// Catch-all for errors padrão --------------------------.
+// const onTurnErrorHandler = async (context, error) => {
+//     // This check writes out errors to console log .vs. app insights.
+//     // NOTE: In production environment, you should consider logging this to Azure
+//     //       application insights.
+//     console.error(`\n [onTurnError] unhandled error: ${ error }`);
+
+//     // Send a trace activity, which will be displayed in Bot Framework Emulator
+//     await context.sendTraceActivity(
+//         'OnTurnError Trace',
+//         `${ error }`,
+//         'https://www.botframework.com/schemas/error',
+//         'TurnError'
+//     );
+
+//     // Send a message to the user
+//     await context.sendActivity('The bot encountered an error or bug.');
+//     await context.sendActivity('To continue to run this bot, please fix the bot source code.');
+// };
+
 // Set the onTurnError for the singleton BotFrameworkAdapter.
-adapter.onTurnError = onTurnErrorHandler;
+// adapter.onTurnError = onTurnErrorHandler;
 
 // Create the main dialog.
-const myBot = new EchoBot();
+const myBot = new DispatchBot();
 
 // Listen for incoming requests.
 server.post('/api/messages', (req, res) => {
@@ -67,18 +96,18 @@ server.post('/api/messages', (req, res) => {
 });
 
 // Listen for Upgrade requests for Streaming.
-server.on('upgrade', (req, socket, head) => {
-    // Create an adapter scoped to this WebSocket connection to allow storing session data.
-    const streamingAdapter = new BotFrameworkAdapter({
-        appId: process.env.MicrosoftAppId,
-        appPassword: process.env.MicrosoftAppPassword
-    });
-    // Set onTurnError for the BotFrameworkAdapter created for each connection.
-    streamingAdapter.onTurnError = onTurnErrorHandler;
+// server.on('upgrade', (req, socket, head) => {
+//     // Create an adapter scoped to this WebSocket connection to allow storing session data.
+//     const streamingAdapter = new BotFrameworkAdapter({
+//         appId: process.env.MicrosoftAppId,
+//         appPassword: process.env.MicrosoftAppPassword
+//     });
+//     // Set onTurnError for the BotFrameworkAdapter created for each connection.
+//     streamingAdapter.onTurnError = onTurnErrorHandler;
 
-    streamingAdapter.useWebSocket(req, socket, head, async (context) => {
-        // After connecting via WebSocket, run this logic for every request sent over
-        // the WebSocket connection.
-        await myBot.run(context);
-    });
-});
+//     streamingAdapter.useWebSocket(req, socket, head, async (context) => {
+//         // After connecting via WebSocket, run this logic for every request sent over
+//         // the WebSocket connection.
+//         await myBot.run(context);
+//     });
+// });
